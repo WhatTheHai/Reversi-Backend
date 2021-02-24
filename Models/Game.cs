b8d022ea4e1718bcee4276a938594eae165be743
WhatTheHai <haiboy@hotmail.nl>
2021-02-25 00:08:39 +0100

    Level 1 progress

--- Models/Game.cs
+++ Models/Game.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace ReversiRestApi.Models
 {
@@ -30,12 +31,45 @@ namespace ReversiRestApi.Models
             throw new NotImplementedException();
         }
 
-        public Colour WinningColour() {
-            throw new NotImplementedException();
+        public Colour WinningColour()
+        {
+            int black, white;
+            black = white = 0;
+            for (int i = 0; i < BoardSize; i++) {
+                for (int j = 0; j < BoardSize; j++) {
+                    switch (Board[i, j])
+                    {
+                        case Colour.None:
+                            break;
+                        case Colour.Black:
+                            black++;
+                            break;
+                        case Colour.White:
+                            white++;
+                            break;
+                    }
+                }
+            }
+            if (black > white)
+                return Colour.Black;
+            return white > black ? Colour.White : Colour.None;
         }
 
+
         public bool PossibleMove(int rowMove, int columnMove) {
-            throw new NotImplementedException();
+            if (rowMove < 0 || rowMove > 7 || columnMove < 0 || columnMove > 7)
+            {
+                return false;
+            }
+
+            var vectors = new List<int> {-1, 0, 1};
+            foreach (int col in vectors) {
+                foreach (int row in vectors) {
+
+                }
+            }
+
+            return true;
         }
 
         public bool DoMove(int rowMove, int columnMove) {
@@ -50,7 +84,7 @@ namespace ReversiRestApi.Models
 
                     Board[row, col] = Colour.None; //Gets overwritten if it has to be black/white
 
-                    if (row == col && (row == halfSz - 1 || col == halfSz - 1))
+                    if (row == col && (row == halfSz - 1 || row == halfSz))
                     {
                         Board[row, col] = Colour.Black;
                     }
@@ -61,8 +95,17 @@ namespace ReversiRestApi.Models
                     }
 
                 }
-            } 
+            }
+        }
 
+        public void ChangeTurns()
+        {
+            IsTurn = (IsTurn == Colour.White) ? Colour.Black : Colour.White;
+        }
+
+        public Colour OppositeColour(Colour colour)
+        {
+            return (colour == Colour.White) ? Colour.Black : Colour.White;
         }
     }
 }
